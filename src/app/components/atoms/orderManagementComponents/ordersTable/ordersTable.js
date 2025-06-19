b2e510dd27fd85837bfa7433c8b2a58b18f691fd
AJ Adversalo <AAdversalo@centrawindows.com>
2025-06-19 11:07:12 -0700

    Order table cleanup

--- src/app/components/atoms/orderManagementComponents/ordersTable/ordersTable.js
+++ src/app/components/atoms/orderManagementComponents/ordersTable/ordersTable.js
@@ -1,14 +1,13 @@
 import { Pagination, Table } from "antd";
 import styles from "./ordersTable.module.css";
-import { Button, DropdownButton } from "react-bootstrap";
-import Dropdown from "react-bootstrap/Dropdown";
+import { Button } from "react-bootstrap";
+
 import {
   updatePageNumber,
   updatePageSize,
   updateSortOrder,
 } from "app/redux/orders";
 import { useDispatch, useSelector } from "react-redux";
-import { current } from "@reduxjs/toolkit";
 
 export default function OrdersTable(props) {
   const {
@@ -21,7 +20,6 @@ export default function OrdersTable(props) {
   } = props;
   const dispatch = useDispatch();
   const onSelectChange = (newSelectedRowKeys) => {
-    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
     setSelectedRows(newSelectedRowKeys);
   };
 
@@ -49,75 +47,41 @@ export default function OrdersTable(props) {
   };
 
   return (
-    <div className={styles.outerContainer}>
-      <div className={styles.innerContainer}>
-        <div className="flex flex-col space-y-2">
-          <div className="flex justify-between items-center sticky">
-            <div className="flex space-x-2 sticky">
-              <Button size="sm" className="text-sm" onClick={onCreateClick}>
-                {/* <i className="fa-solid fa-plus pr-2" /> */}
-                <span>Create</span>
-              </Button>
-              {/* <Badge color={"#007BFF"} count={selectedRows.length}>
-                <Tooltip title="Actions">
-                  <DropdownButton
-                    id="dropdown-basic-button-lite"
-                    size="md"
-                    title={
-                      <span className="w-full pr-2 font-semibold">Actions</span>
-                    }
-                    className="flex justify-between w-full hover:bg-[#f9f9f9] rounded"
-                  >
-                    <Dropdown.Item
-                      onClick={() => {}}
-                      disabled={selectedRows.length === 0}
-                    >
-                      <div className="flex items-center text-sm w-full">
-                        <i className="fa-regular fa-calendar"></i>
-                        <span className="pl-2">Reschedule</span>
-                      </div>
-                    </Dropdown.Item>
-                    <Dropdown.Item
-                      onClick={() => {}}
-                      disabled={selectedRows.length === 0}
-                    >
-                      <div className="flex items-center text-sm w-full">
-                        <i className="fa-regular fa-pen-to-square"></i>
-                        <span className="pl-2">Update Status</span>
-                      </div>
-                    </Dropdown.Item>
-                  </DropdownButton>
-                </Tooltip>
-              </Badge> */}
-            </div>
-
-            <div className="flex justify-end items-center">
-              <Pagination
-                //showSizeChanger
-                onChange={onChangeProps}
-                total={total}
-                showTotal={(total) => (
-                  <div className="text-sm font-semibold mt-2">{` ${total.toLocaleString()} Total`}</div>
-                )}
-                current={pageNumber}
-                pageSize={pageSize}
-                //pageSizeOptions={[20]}
-              />
-            </div>
+    <div className={"bg-white rounded-sm p-3"}>
+      <div className="flex flex-col space-y-2">
+        <div className="flex justify-between items-center sticky">
+          <div className="flex space-x-2 sticky">
+            <Button size="sm" className="text-sm" onClick={onCreateClick}>
+              {/* <i className="fa-solid fa-plus pr-2" /> */}
+              <span>Create</span>
+            </Button> 
           </div>
-          <div style={{ height: "calc(100vh - 220px)" }}>
-            <Table
-              columns={columns}
-              dataSource={data}
-              size="small"
-              pagination={false}
-              //rowSelection={rowSelection}
-              loading={isLoading}
-              scroll={{ y: "calc(100vh - 270px)" }}
-              style={{ height: "100%" }}
-              onChange={onTableChange}
+
+          <div className="flex justify-end items-center">
+            <Pagination
+              //showSizeChanger
+              onChange={onChangeProps}
+              total={total}
+              showTotal={(total) => (
+                <div className="text-sm font-semibold mt-2">{` ${total.toLocaleString()} Total`}</div>
+              )}
+              current={pageNumber}
+              pageSize={pageSize}
+            //pageSizeOptions={[20]}
             />
           </div>
+        </div>
+        <div style={{ height: "calc(100vh - 195px)" }} className="overflow-auto">
+          <Table
+            columns={columns}
+            dataSource={data}
+            size="small"
+            pagination={false}
+            //rowSelection={rowSelection}
+            loading={isLoading}            
+            onChange={onTableChange}
+            sticky
+          />
         </div>
       </div>
     </div>
