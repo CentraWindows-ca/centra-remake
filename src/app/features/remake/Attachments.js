9fb0176d6a08c9bd02967021509307af38426d3f
AJ Adversalo <AAdversalo@centrawindows.com>
2025-06-26 10:26:42 -0700

    2817 - Attachment View / Delete / Download

--- src/app/features/remake/Attachments.js
+++ src/app/features/remake/Attachments.js
@@ -1,20 +1,19 @@
 "use client";
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 import {
   uploadAttachments,
+  deleteAttachments
 } from "app/api/genericApis/attachmentsApi";
 
 import { useQuery } from "react-query";
 
-import { Empty, message, Image } from "antd";
+import { Empty, message, Image, Popover, Popconfirm } from "antd";
 
 import {
   fetchAttachments,
 } from "app/api/genericApis/attachmentsApi";
 import AntUploadModalWithNotes from "app/components/antUploadModalWithNotes/antUploadModalWithNotes";
-
-import { saveAs } from "file-saver";
 
 export default function Attachments(props) {
   const {
@@ -65,7 +64,6 @@ export default function Attachments(props) {
 
       if (payload?.length > 0) {
         const result = await uploadAttachments("remake", orderId, payload);
-        console.log("upload result ", result);
         if (result) {
           message.success("Attachments uploaded successfully.");
           refetchAttachments();
@@ -93,6 +91,69 @@ export default function Attachments(props) {
     }
     return <i className="fa-regular fa-file text-gray-400 pt-1" />;
   };
+
+
+  const handleView = (attachment) => {
+    const base64 = attachment.base64;
+    const mimeType = attachment.mimeType;
+
+    // Remove "data:*/*;base64," prefix if it exists
+    const base64Data = base64.includes(',') ? base64.split(',')[1] : base64;
+
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length).fill().map((_, i) => byteCharacters.charCodeAt(i));
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: mimeType });
+
+    const blobUrl = URL.createObjectURL(blob);
+    window.open(blobUrl, '_blank');
+  };
+
+  const handleDelete = async (id) => {
+    const result = await deleteAttachments("remake", [id])
+    if (result) {
+      message.success("Attachment deleted successfully.");
+      refetchAttachments();     
+    } else {
+      message.error("Failed to delete attachment.");
+    }
+  }
+
+  const handleDownload = (attachment) => {
+    console.log("attachment ", attachment)
+    const link = document.createElement("a");
+    link.href = attachment.base64.startsWith("data:")
+      ? attachment.base64
+      : `data:application/octet-stream;base64,${attachment.base64}`;
+    link.download = attachment.name;
+    link.click();
+  }
+
+  const AttachmentContent = (attachment) => {
+    return (
+      <div>
+        <div className="border-b pb-1 mb-1 max-w-[12rem] font-semibold">{`${attachment?.name} (${attachment?.size})`}</div>
+        <div onClick={() => handleDownload(attachment)}><i className="fa-solid fa-download pr-2 text-blue-500" /><span className="hover:underline hover:text-blue-500 hover:cursor-pointer">Download</span></div>
+        <div onClick={() => handleView(attachment)}><i className="fa-solid fa-eye pr-2 text-green-500" /><span className="hover:underline hover:text-blue-500 hover:cursor-pointer">View</span></div>        
+        <div><i className="fa-solid fa-pen pr-2 text-blue-500" /><span className="hover:underline hover:text-blue-500 hover:cursor-pointer">Edit</span></div>
+        <Popconfirm
+          title="Delete Attachment"
+          description="Are you sure to delete this file?"
+          onConfirm={() => handleDelete(attachment.id)}
+          onCancel={() => { }}
+          okText="Yes"
+          cancelText="No"
+        >
+          <div><i className="fa-solid fa-trash-can pr-2 text-red-500" /><span className="hover:underline hover:text-blue-500 hover:cursor-pointer">Delete</span></div>
+        </Popconfirm>
+      </div>
+    )
+  }
+
+
+  useEffect(() => {
+    console.log("attachments ", attachments)
+  }, [attachments])
 
   return (
     <>
@@ -131,36 +192,21 @@ export default function Attachments(props) {
                   <div className="flex flex-row justify-between group">
                     <div
                       className="group-hover:underline group-hover:cursor-pointer"
-                      onClick={() => {
-                        //if (isImage) {
-                        // setPreviewVisible(true);
-                        //} else {
-                        const dataUri = attachment.base64.startsWith('data:')
-                          ? attachment.base64
-                          : `data:${attachment.mimeType};base64,${attachment.base64}`;
-
-                        // Open file in a new tab using iframe
-                        const newTab = window.open();
-                        if (newTab) {
-                          newTab.document.write(`
-                              <html>
-                                <head><title>Preview: ${attachment.name}</title></head>
-                                <body style="margin:0">
-                                  <iframe src="${dataUri}" style="border:0;width:100vw;height:100vh;"></iframe>
-                                </body>
-                              </html>
-                            `);
-                        }
-                        //}
-                      }}
+                      onClick={() => handleView(attachment)}
                     >
                       <div className="flex flex-row">
                         <div className="pr-1">{getFileIcon(attachment?.mimeType)}</div>
-                        <div className="w-[9.5rem]">{attachment.fileNotes || attachment.name}</div>
+                        <div className="w-[9.5rem] hover:text-blue-500">{attachment.fileNotes || attachment.name}</div>
                       </div>                      
                       {false && <span className="pl-1 text-blue-500">({attachment.size})</span>}
                     </div>
-                    <i class="mt-[5px] fa-solid fa-gear text-gray-400 group-hover:cursor-pointer hover:text-blue-500" />
+                    <Popover
+                      trigger="hover"
+                      content={AttachmentContent(attachment)}
+                      placement={"right"}
+                    >
+                      <i className="mt-[5px] fa-solid fa-gear text-gray-400 group-hover:cursor-pointer hover:text-blue-500"/>
+                    </Popover>
                   </div>
                 </>)
             })}
@@ -189,37 +235,19 @@ export default function Attachments(props) {
           'image/webp',
           'image/heic',
           'image/svg+xml',
-
           // PDFs
           'application/pdf',
-
           // Word documents
           'application/msword',                     // .doc
           'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // .docx
-
           // Excel files
           'application/vnd.ms-excel',               // .xls
           'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // .xlsx
-
           // PowerPoint
           'application/vnd.ms-powerpoint',          // .ppt
           'application/vnd.openxmlformats-officedocument.presentationml.presentation', // .pptx
-
           // Text files
           'text/plain',
-
-          // CSV, JSON
-          'text/csv',
-          'application/json',
-
-          // Archives
-          'application/zip',
-          'application/x-7z-compressed',
-          'application/x-rar-compressed',
-          'application/x-tar',
-
-          // Rich Text Format
-          'application/rtf'
         ]}
         title={"Upload Attachments"}
         canUpload={true}
